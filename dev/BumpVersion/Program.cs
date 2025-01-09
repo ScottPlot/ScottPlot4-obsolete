f0169a03f08464af1af9ff57883c9347de3e1f5b
Scott W Harden <swharden@gmail.com>
2025-01-08 19:02:41 -0500

    dev: delete obsolete tools

--- dev/BumpVersion/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿string repoFolder = Locate.GetRepoRootFolder();
-string[] csprojFilePaths = Locate.GetCsprojFilePaths(repoFolder);
-
-foreach (string csprojFilePath in csprojFilePaths)
-{
-    CsprojEditor.BumpVersion(csprojFilePath);
-}
--- dev/CodeAnalysis/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using CodeAnalysis;
-
-string repoRoot = Path.GetFullPath(Path.Join(AppDomain.CurrentDomain.BaseDirectory, "../../../../../"));
-
-string saveAs = Path.Combine(repoRoot, "dev/www/metrics/index.html");
-CodeReport.Generate(repoRoot, saveAs);
